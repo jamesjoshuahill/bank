81b8fd842b63b04e7b0f21008dcc14e7b257865e
Josh Hill <jamesjoshuahill@users.noreply.github.com>
2020-04-02 18:05:44 +0100

    Account can deposit an amount

--- lib/account.rb
+++ lib/account.rb
@@ -1,9 +1,14 @@
 class Account
   def initialize(statement_printer)
     @statement_printer = statement_printer
+    @transactions = []
   end
 
   def print_statement
-    @statement_printer.print([])
+    @statement_printer.print(@transactions)
+  end
+
+  def deposit(amount, date)
+    @transactions.push({ date: date, credit: amount })
   end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,14 +1,22 @@
 require "account"
 
 RSpec.describe Account do
-  let(:statement_printer) { double(:statement_printer) }
+  let(:statement_printer) { double(:statement_printer, print: nil) }
   subject { described_class.new(statement_printer) }
 
   it "can print an empty statement" do
-    allow(statement_printer).to receive(:print)
-
     subject.print_statement
 
     expect(statement_printer).to have_received(:print).with([])
+  end
+
+  it "can deposit an amount" do
+    subject.deposit(1000, Date.new(2012, 1, 10))
+
+    subject.print_statement
+    expect(statement_printer).to have_received(:print).with([{
+      date: Date.new(2012, 1, 10),
+      credit: 1000
+    }])
   end
 end
