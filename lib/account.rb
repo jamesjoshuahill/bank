bb3a60aaa3365d1b8d98f718f000a0cdeb1490a9
Josh Hill <jamesjoshuahill@users.noreply.github.com>
2020-04-08 15:25:00 +0100

    Refactor Account to use TransactionRepository
    - Remove @transactions and @transaction_class from Account

--- lib/account.rb
+++ lib/account.rb
@@ -1,24 +1,26 @@
 require_relative "statement_printer"
-require_relative "transaction"
+require_relative "transaction_repository"
 
 class Account
   class InsufficientFundsError < StandardError; end
   class InvalidAmountError < StandardError; end
 
-  def initialize(statement_printer = StatementPrinter.new, transaction_class = Transaction)
+  def initialize(statement_printer = StatementPrinter.new,
+    transaction_repository = TransactionRepository.new)
+
     @statement_printer = statement_printer
-    @transaction_class = transaction_class
-    @transactions = []
+    @transaction_repository = transaction_repository
   end
 
   def print_statement
-    @statement_printer.print(@transactions)
+    transactions = @transaction_repository.all
+    @statement_printer.print(transactions)
   end
 
   def deposit(amount, date)
     validate_amount(amount)
     balance = current_balance + amount
-    create_transaction(date: date, credit: amount, balance: balance)
+    @transaction_repository.create(date: date, credit: amount, balance: balance)
   end
 
   def withdraw(amount, date)
@@ -26,27 +28,16 @@ class Account
     raise InsufficientFundsError if amount > current_balance
 
     balance = current_balance - amount
-    create_transaction(date: date, debit: amount, balance: balance)
+    @transaction_repository.create(date: date, debit: amount, balance: balance)
   end
 
   private
 
   def current_balance
-    @transactions.any? ? @transactions.last.balance : 0
+    @transaction_repository.current_balance
   end
 
   def validate_amount(amount)
     raise InvalidAmountError if amount < 1
-  end
-
-  def create_transaction(date:, credit: 0, debit: 0, balance:)
-    transaction = @transaction_class.new(
-      date: date,
-      credit: credit,
-      debit: debit,
-      balance: balance
-    )
-    @transactions.push(transaction)
-    return transaction
   end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,10 +2,12 @@ require "account"
 
 RSpec.describe Account do
   let(:statement_printer) { instance_double(StatementPrinter, print: nil) }
-  let(:transaction_class) { class_double(Transaction) }
-  subject { described_class.new(statement_printer, transaction_class) }
+  let(:transaction_repository) { instance_double(TransactionRepository) }
+  subject { described_class.new(statement_printer, transaction_repository) }
 
   it "can print an empty statement" do
+    allow(transaction_repository).to receive(:all).and_return([])
+
     subject.print_statement
 
     expect(statement_printer).to have_received(:print).with([])
@@ -13,43 +15,36 @@ RSpec.describe Account do
 
   it "can deposit an amount" do
     deposit = instance_double(Transaction)
-    allow(transaction_class).to receive(:new)
-      .with(date: Date.new(2012, 1, 10), credit: 1000, debit: 0, balance: 1000)
+    allow(transaction_repository).to receive(:create)
+      .with(date: Date.new(2012, 1, 10), credit: 1000, balance: 1000)
       .and_return(deposit)
+    allow(transaction_repository).to receive(:current_balance).and_return(0)
 
     transaction = subject.deposit(1000, Date.new(2012, 1, 10))
 
     expect(transaction).to eq(deposit)
-    subject.print_statement
-    expect(statement_printer).to have_received(:print).with([deposit])
   end
 
   it "can deposit twice" do
-    deposit_1 = instance_double(Transaction, balance: 1000)
+    deposit_1 = instance_double(Transaction)
     deposit_2 = instance_double(Transaction)
-    allow(transaction_class).to receive(:new).and_return(deposit_1, deposit_2)
+    allow(transaction_repository).to receive(:create).and_return(deposit_1, deposit_2)
+    allow(transaction_repository).to receive(:current_balance).and_return(0, 1000)
 
     subject.deposit(1000, Date.new(2012, 1, 10))
     subject.deposit(2000, Date.new(2012, 1, 13))
-
-    subject.print_statement
-    expect(statement_printer).to have_received(:print).with([deposit_1, deposit_2])
   end
 
   it "can withdraw less than the balance" do
-    deposit = instance_double(Transaction, balance: 2000)
-    allow(transaction_class).to receive(:new).and_return(deposit)
     withdrawal = instance_double(Transaction)
-    allow(transaction_class).to receive(:new)
-      .with(date: Date.new(2012, 1, 14), credit: 0, debit: 500, balance: 1500)
+    allow(transaction_repository).to receive(:create)
+      .with(date: Date.new(2012, 1, 14), debit: 500, balance: 1500)
       .and_return(withdrawal)
+    allow(transaction_repository).to receive(:current_balance).and_return(2000)
 
-    subject.deposit(2000, Date.new(2012, 1, 13))
     transaction = subject.withdraw(500, Date.new(2012, 1, 14))
 
     expect(transaction).to eq(withdrawal)
-    subject.print_statement
-    expect(statement_printer).to have_received(:print).with([deposit, withdrawal])
   end
 
   it "fails to deposit a zero amount" do
@@ -73,6 +68,8 @@ RSpec.describe Account do
   end
 
   it "fails to withdraw more than the balance" do
+    allow(transaction_repository).to receive(:current_balance).and_return(0)
+
     expect { subject.withdraw(1, Date.new(2012, 1, 10)) }
       .to raise_error(Account::InsufficientFundsError)
   end
